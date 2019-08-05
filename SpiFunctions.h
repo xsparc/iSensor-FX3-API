d8f5a729f22cb5ffe99e7d9dbbb6aca9074ed1d9
Nolan <Alex.Nolan@analog.com>
2019-08-05 16:05:02 -0400

    Removed deprecated functions from header

--- SpiFunctions.h
+++ SpiFunctions.h
@@ -32,8 +32,5 @@ CyU3PReturnStatus_t AdiTransferBytes(uint32_t writeData);
 CyU3PReturnStatus_t AdiWriteRegByte(uint16_t addr, uint8_t data);
 CyU3PReturnStatus_t AdiReadRegBytes(uint16_t addr);
 CyU3PReturnStatus_t AdiBulkByteTransfer(uint16_t numBytes, uint16_t bytesPerCapture);
-CyU3PReturnStatus_t AdiWritePageReg(uint16_t pageNumber);
-CyU3PReturnStatus_t AdiReadSpiReg(uint16_t address, uint16_t page, uint16_t numBytes, uint8_t  *buffer);
-CyU3PReturnStatus_t AdiWriteSpiReg(uint16_t address, uint16_t page, uint16_t numBytes, uint8_t  *buffer);
 
 #endif
