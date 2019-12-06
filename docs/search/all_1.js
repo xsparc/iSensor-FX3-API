5dca607d3aef74f1a9f20a1328e3c7419f8a9c99
anolan3 <Alex.Nolan@analog.com>
2019-12-06 13:46:02 -0500

    Rebuilt docs

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -3,6 +3,7 @@ var searchData=
   ['bitbang_5fhalfclock_5foffset',['BITBANG_HALFCLOCK_OFFSET',['../_spi_functions_8h.html#a0871091ac711260182300d99d852b859',1,'SpiFunctions.h']]],
   ['bitbangspiconf',['BitBangSpiConf',['../struct_bit_bang_spi_conf.html',1,'BitBangSpiConf'],['../_spi_functions_8h.html#ad5767ba5fb928632201d3cb2e4dce9e0',1,'BitBangSpiConf():&#160;SpiFunctions.h']]],
   ['boardstate',['BoardState',['../struct_board_state.html',1,'BoardState'],['../main_8h.html#a276ba3fa12d3373c2e4e1db2ea3a70e7',1,'BoardState():&#160;main.h']]],
+  ['boottime',['BootTime',['../struct_board_state.html#ac38aa90dc9478f88c30f00e1d37b5c94',1,'BoardState']]],
   ['bulkbuffer',['BulkBuffer',['../main_8c.html#add07376eecfcd79dfd9f998172675538',1,'main.c']]],
   ['busypin',['BusyPin',['../struct_board_state.html#ab906b9042e21ba1d46670dff07feda01',1,'BoardState']]],
   ['bytesperbuffer',['BytesPerBuffer',['../struct_stream_state.html#ad0d93b9b964fc625ed26e0709ab4ac99',1,'StreamState']]],
--- docs/search/variables_1.js
+++ docs/search/variables_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['boottime',['BootTime',['../struct_board_state.html#ac38aa90dc9478f88c30f00e1d37b5c94',1,'BoardState']]],
   ['busypin',['BusyPin',['../struct_board_state.html#ab906b9042e21ba1d46670dff07feda01',1,'BoardState']]],
   ['bytesperbuffer',['BytesPerBuffer',['../struct_stream_state.html#ad0d93b9b964fc625ed26e0709ab4ac99',1,'StreamState']]],
   ['bytesperframe',['BytesPerFrame',['../struct_stream_state.html#a3537c778f2105ffbfc3859c556163a80',1,'StreamState']]],
