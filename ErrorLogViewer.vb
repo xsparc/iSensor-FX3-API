cd2c5bdcfa373938297247b29910b6c4df6891c5
anolan3 <Alex.Nolan@analog.com>
2019-12-06 13:51:19 -0500

    Added error log viewer class

---
+++ ErrorLogViewer.vb
@@ -0,0 +1,11 @@
+﻿
+''' <summary>
+''' This class provides an interface to retrieve error logs from a connected FX3
+''' </summary>
+Public Class ErrorLogViewer
+
+    Public Sub New(ByRef FX3 As FX3Connection)
+
+    End Sub
+
+End Class
