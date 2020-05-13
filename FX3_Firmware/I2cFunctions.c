1244a640d80ef19484db9ae8f5d5db2f62987601
anolan3 <Alex.Nolan@analog.com>
2020-05-13 12:20:44 -0400

    Added i2c interface module files

---
+++ FX3_Firmware/I2cFunctions.c
@@ -0,0 +1,20 @@
+/**
+  * Copyright (c) Analog Devices Inc, 2018 - 2020
+  * All Rights Reserved.
+  *
+  * THIS SOFTWARE UTILIZES LIBRARIES DEVELOPED
+  * AND MAINTAINED BY CYPRESS INC. THE LICENSE INCLUDED IN
+  * THIS REPOSITORY DOES NOT EXTEND TO CYPRESS PROPERTY.
+  *
+  * Use of this file is governed by the license agreement
+  * included in this repository.
+  *
+  * @file		I2cFunctions.c
+  * @date		5/13/2020
+  * @author		A. Nolan (alex.nolan@analog.com)
+  * @brief 		Implementation file for USB-I2C interfacing module
+ **/
+
+#include "I2cFunctions.h"
+
+
---
+++ FX3_Firmware/I2cFunctions.h
@@ -0,0 +1,24 @@
+/**
+  * Copyright (c) Analog Devices Inc, 2018 - 2020
+  * All Rights Reserved.
+  *
+  * THIS SOFTWARE UTILIZES LIBRARIES DEVELOPED
+  * AND MAINTAINED BY CYPRESS INC. THE LICENSE INCLUDED IN
+  * THIS REPOSITORY DOES NOT EXTEND TO CYPRESS PROPERTY.
+  *
+  * Use of this file is governed by the license agreement
+  * included in this repository.
+  *
+  * @file		I2cFunctions.c
+  * @date		5/13/2020
+  * @author		A. Nolan (alex.nolan@analog.com)
+  * @brief 		Header file for USB-I2C interfacing module
+ **/
+
+
+#ifndef I2CFUNCTIONS_H_
+#define I2CFUNCTIONS_H_
+
+
+
+#endif /* I2CFUNCTIONS_H_ */
