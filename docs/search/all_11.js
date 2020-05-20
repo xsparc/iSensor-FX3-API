3b54e6aa0f680c8055409201b7f6448a2cba2eaa
anolan3 <Alex.Nolan@analog.com>
2020-05-20 17:50:06 -0400

    Rebuild docs

--- docs/search/all_11.js
+++ docs/search/all_11.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['unknown',['Unknown',['../_error_log_8h.html#a961bfce4003ed3e90785c485090db4f7a4e81c184ac3ad48a389cd4454c4a05bb',1,'ErrorLog.h']]],
+  ['uptime',['Uptime',['../struct_error_msg.html#a051fb7ab47885f9a441cf70ddc713558',1,'ErrorMsg']]],
   ['usbbuffer',['USBBuffer',['../_flash_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;Flash.c'],['../_i2c_functions_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;I2cFunctions.c'],['../_pin_functions_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;PinFunctions.c'],['../_spi_functions_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;SpiFunctions.c'],['../_stream_functions_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;StreamFunctions.c'],['../_stream_thread_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;StreamThread.c'],['../main_8c.html#aae2dfaf6e49d51d401e281fbdf83d874',1,'USBBuffer((aligned(32))):&#160;main.c']]],
   ['usbbuffersize',['UsbBufferSize',['../struct_board_state.html#a7ecc16e123ad5c0963d236b46e7974c1',1,'BoardState']]]
 ];
--- docs/search/variables_11.js
+++ docs/search/variables_11.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['uptime',['Uptime',['../struct_error_msg.html#a051fb7ab47885f9a441cf70ddc713558',1,'ErrorMsg']]],
   ['usbbuffer',['USBBuffer',['../_flash_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;Flash.c'],['../_i2c_functions_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;I2cFunctions.c'],['../_pin_functions_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;PinFunctions.c'],['../_spi_functions_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;SpiFunctions.c'],['../_stream_functions_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;StreamFunctions.c'],['../_stream_thread_8c.html#af61156727a983c3f2ebfd9ae0e5bac71',1,'USBBuffer():&#160;StreamThread.c']]],
   ['usbbuffersize',['UsbBufferSize',['../struct_board_state.html#a7ecc16e123ad5c0963d236b46e7974c1',1,'BoardState']]]
 ];
