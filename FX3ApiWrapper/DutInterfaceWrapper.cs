c11f78537d69c94409903663fece319fd09a2e83
anolan3 <Alex.Nolan@analog.com>
2020-06-05 15:19:25 -0400

    Added FX3 API wrapper class

---
+++ FX3ApiWrapper/DutInterfaceWrapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FX3Api;
+using RegMapClasses;
+using adisInterface;
+
+namespace FX3ApiWrapper
+{
+    public class DutInterfaceWrapper
+    {
+        private FX3Connection m_FX3;
+        private RegMapCollection m_RegMap;
+        private IDutInterface m_Dut;
+
+        public DutInterfaceWrapper(FX3Connection FX3, string RegMapPath)
+        {
+            m_FX3 = FX3;
+            UpdateDutType();
+            UpdateRegMap(RegMapPath);
+        }
+
+        public void UpdateDutType()
+        {
+            if(m_FX3.SensorType == DeviceType.ADcmXL)
+            {
+                if(m_FX3.PartType == DUTType.ADcmXL1021)
+                {
+                    m_Dut = new AdcmInterface1Axis(m_FX3, null);
+                }
+                else if(m_FX3.PartType == DUTType.ADcmXL2021)
+                {
+                    m_Dut = new AdcmInterface2Axis(m_FX3, null);
+                }
+                else
+                {
+                    /* 3021 or other */
+                    m_Dut = new AdcmInterface3Axis(m_FX3, null);
+                }
+            }
+            else if(m_FX3.SensorType == DeviceType.AutomotiveSpi)
+            {
+                m_Dut = new ZeusInterface(m_FX3, null);
+            }
+            else
+            {
+                if(m_FX3.PartType == DUTType.LegacyIMU)
+                {
+                    m_Dut = new aducInterface(m_FX3, null);
+                }
+                else
+                {
+                    m_Dut = new adbfInterface(m_FX3, null);
+                }
+            }
+        }
+
+        public void UpdateRegMap(string RegMapPath)
+        {
+            m_RegMap = new RegMapCollection();
+            m_RegMap.ReadFromCSV(RegMapPath);
+        }
+    }
+}
---
+++ FX3ApiWrapper/FX3Wrapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FX3Api;
+
+namespace FX3ApiWrapper
+{
+    public class FX3Wrapper
+    {
+        /// <summary>
+        /// Underlying FX3 connection object
+        /// </summary>
+        public FX3Connection FX3;
+
+        public FX3Wrapper(string FX3ResourcePath)
+        {
+            FX3 = new FX3Connection(FX3ResourcePath, FX3ResourcePath, FX3ResourcePath);
+            ConnectToBoard();
+        }
+
+        public void Disconnect()
+        {
+            FX3.Disconnect();
+        }
+
+        private void ConnectToBoard()
+        {
+            FX3.WaitForBoard(2);
+            if(FX3.AvailableFX3s.Count() > 0)
+            {
+                FX3.Connect(FX3.AvailableFX3s[0]);
+            }
+            else if(FX3.BusyFX3s.Count() > 0)
+            {
+                FX3.ResetAllFX3s();
+                FX3.WaitForBoard(5);
+                ConnectToBoard();
+            }
+            else
+            {
+                throw new Exception("No FX3 board connected!");
+            }
+        }
+    }
+}
