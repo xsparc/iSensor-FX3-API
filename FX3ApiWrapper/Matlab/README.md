7e4bc447dc5f52c95fb95bc5fa427e469b9c9de7
anolan3 <Alex.Nolan@analog.com>
2020-06-08 17:07:41 -0400

    Made matlab example much fancier

--- FX3ApiWrapper/Matlab/README.md
+++ FX3ApiWrapper/Matlab/README.md
@@ -1,3 +1,5 @@
-This simple Matlab script connects to an FX3 and reads all the primary output registers from an ADIS1650x DUT.
+This simple Matlab script connects to an FX3 and reads the primary XL output registers from an ADIS1650x DUT. Each set of reads are data ready synchronized, and the resulting data is FFT'd and plotted.
+
+Reference for .NET <-> Matlab data conversions can be found here: https://www.mathworks.com/help/matlab/matlab_external/passing-net-data-in-matlab.html
 
 Note, any hardcoded resource paths will have to be adjusted to match your system
--- FX3ApiWrapper/Matlab/fx3_api_example.m
+++ FX3ApiWrapper/Matlab/fx3_api_example.m
@@ -6,23 +6,80 @@ Dut = FX3ApiWrapper.Wrapper('C:\Users\anolan3\Documents\iSensor-FX3-API\Resource
     'C:\Users\anolan3\Documents\iSensor-FX3-ExampleGui\src\ADIS1650x_Regmap.csv',...
     FX3ApiWrapper.SensorType.StandardImu);
 
-%Blink user LED at 2Hz
-Dut.UserLEDBlink(2.0);
+%Blink user LED at 5Hz
+Dut.UserLEDBlink(5.0);
 
-%array to hold DUT data
-data = [];
+%enable dr active
+Dut.SetDrActive(true);
+
 %Create reglist
-regs = NET.createArray('System.String',8);
-regs(1) = 'STATUS';
-regs(2) = 'DATA_CNTR';
-regs(3) = 'XGYRO_UPR';
-regs(4) = 'YGYRO_UPR';
-regs(5) = 'ZGYRO_UPR';
-regs(6) = 'XACCL_UPR';
-regs(7) = 'YACCL_UPR';
-regs(8) = 'ZACCL_UPR';
+regs = NET.createArray('System.String',3);
+regs(1) = 'XACCL_UPR';
+regs(2) = 'YACCL_UPR';
+regs(3) = 'ZACCL_UPR';
+
+%sample freq
+Fs = 2000;
+%sample period
+T = 1/Fs;
+% NFFT
+L = 4096;
+%fft frequency vector
+f = Fs*(0:(L/2 - 1))/L;
+
+%array to hold raw DUT data
+rawData = [];
+
+%data for each accel axis
+x = zeros(1,L);
+y = zeros(1,L);
+z = zeros(1,L);
+
+x_fft = [];
+y_fft = [];
+z_fft = [];
+
+%index in raw data
+i = 1;
+
 while(true)
-    data = int32(Dut.ReadUnsigned(regs));
-    disp(data)
-    pause(0.5)
+    rawData = int32(Dut.ReadSigned(regs, 1, L));
+    i = 1;
+    for n = 1:3:length(rawData)
+        x(i) = rawData(n);
+        y(i) = rawData(n + 1);
+        z(i) = rawData(n + 2);
+        i = i + 1;
+    end
+    
+    %calc FFT
+    x_fft = fft(x);
+    y_fft = fft(y);
+    z_fft = fft(z);
+    
+    %remove back half
+    x_fft = x_fft(1:(L/2));
+    y_fft = y_fft(1:(L/2));
+    z_fft = z_fft(1:(L/2));
+    
+    %Scale to magnitude
+    x_fft = abs(x_fft);
+    y_fft = abs(y_fft);
+    z_fft = abs(z_fft);
+    
+    %divide by fft length
+    x_fft = x_fft / L;
+    y_fft = y_fft / L;
+    z_fft = z_fft / L;
+    
+    loglog(f, x_fft);
+    hold on;
+    loglog(f, y_fft);
+    loglog(f, z_fft);
+    hold off;
+    
+    xlabel('Frequency (in hertz)');
+    title('ADIS1650x XL FFT');
+
+    pause(1)
 end
